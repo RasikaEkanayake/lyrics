f34f26a96caa7c0d23a41d388effddba5771e643
rasika <rasikaekanayaka0000@gmail.com>
2025-01-24 08:34:17 +0530

    Initial Commit

---
+++ lyrics/lyricsApp.swift
@@ -0,0 +1,17 @@
+//
+//  lyricsApp.swift
+//  lyrics
+//
+//  Created by Rasika Ekanayaka on 2025-01-24.
+//
+
+import SwiftUI
+
+@main
+struct lyricsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
